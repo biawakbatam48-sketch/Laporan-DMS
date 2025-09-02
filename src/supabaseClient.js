9f795552e168545b8348e0a6de98048323049bd4
Aranda <biawakbatam48@gmail.com>
2025-09-03 00:17:46 +0800

    hapus fitur login/reg

--- src/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mujwmrliifglftbfasau.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11andtcmxpaWZnbGZ0YmZhc2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MjcxMDUsImV4cCI6MjA3MjQwMzEwNX0.odbLe98iGmcJqoo7Xa-3UaTOIq-TO-VpvhnpkMS54Eo'
-const supabase = createClient(supabaseUrl, supabaseKey)
